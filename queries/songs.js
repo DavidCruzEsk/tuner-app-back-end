7b057dabbc591554959a91d62832623603178cfe
David Cruz Esk <david.cruz.esk@gmail.com>
2024-07-13 16:06:47 -0400

    Create update & delete functions for songs in songs.js | create put and delete routes in songsController.js

--- queries/songs.js
+++ queries/songs.js
@@ -5,7 +5,7 @@ async function getAllSongs() {
         const allSongs = await db.any('SELECT * FROM songs');
         return allSongs;
     } catch (error) {
-        return error;
+        throw error;
     }
 }
 
@@ -14,17 +14,39 @@ async function getSong(id) {
         const song = await db.one('SELECT * FROM songs WHERE id=$1', id);
         return song;
     } catch (error) {
-        return error;
+        throw error;
     }
 }
 
 async function createSong(song) {
-    const newSong = await db.one('INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5)RETURNING *', [song.name, song.artist, song.album, song.time, song.is_favorite]);
+    const newSong = await db.one('INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *', [song.name, song.artist, song.album, song.time, song.is_favorite]);
     return newSong;
+}
+
+async function updateSong(id, song) {
+    try {
+        const query = 'UPDATE songs SET name = $2, artist = $3, album = $4, time = $5, is_favorite = $6 WHERE id = $1 RETURNING *';
+        const values = [id, song.name, song.artist, song.album, song.time, song.is_favorite];
+        const songToUpdate = await db.one(query, values);
+        return songToUpdate;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function deleteSong(id) {
+    try {
+        const unwantedSong = await db.one('DELETE FROM songs WHERE id = $1 RETURNING *', id);
+        return unwantedSong;
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
     getAllSongs,
     getSong,
-    createSong
+    createSong,
+    updateSong,
+    deleteSong
 };
