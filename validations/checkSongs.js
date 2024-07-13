0b20d7e8f9748f0aef0c89876b5275d5fefbbbef
David Cruz Esk <david.cruz.esk@gmail.com>
2024-07-13 13:53:17 -0400

    fourth commit

--- validations/checkSongs.js
+++ validations/checkSongs.js
@@ -1,26 +1,26 @@
 function checkSong (req, res, next) {
     if (!req.body.name) {
         res.status(400).json({ error: 'Name Must Be Included'});
-    }
-
-    if (!req.body.name) {
-        res.status(400).json({ error: 'Name Must Be Included'});
+    } else if (typeof req.body.name !== 'string') {
+        res.status(400).json({ error: 'Name value must be a string'});
     }
 
     if (!req.body.artist) {
         res.status(400).json({ error: 'Artist Must Be Included'});
+    } else if (typeof req.body.name !== 'string') {
+        res.status(400).json({ error: 'Name value must be a string'});
     }
 
-    if (!req.body.album) {
-        res.status(400).json({ error: 'Album Must Be Included'});
+    if (req.body.alum && typeof req.body.album !== 'string') {
+        res.status(400).json({ error: 'Album value must be a string'});
     }
 
-    if (!req.body.time) {
-        res.status(400).json({ error: 'Time Must Be Included'});
+    if (req.body.time && typeof req.body.time !== 'string') {
+        res.status(400).json({ error: 'Time value must be a string'});
     }
 
-    if (!req.body.is_favorite) {
-        res.status(400).json({ error: 'is_favorite Must Be Included'});
+    if (req.body.is_favorite && typeof req.body.is_favorite !== 'boolean') {
+        res.status(400).json({ error: 'is_favorite value must be a boolean'});
     }
 
     next();
